f501b49a9ca0ac2d81a669062ba33bab3d831932
DingAlessandro <121311960+DingAlessandro@users.noreply.github.com>
2024-03-05 09:16:39 +0100

    Add files via upload

---
+++ GiocoDadi_Ding_Negrello_Formaggio/Dado.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiocoDadi
+{
+    internal class Dado
+    {
+        string path;
+        Random random;
+        int value;
+        public Dado()
+        {
+            path = Environment.CurrentDirectory;
+            random = new Random();
+        }
+
+        public string tiraDado() 
+        {
+            value = random.Next(1, 7);
+            path = Environment.CurrentDirectory;
+            path = path + "\\" + value + ".jpg";
+            return path;
+        }
+
+        static public bool operator > (Dado d1, Dado d2) 
+        {
+            return d1.value > d2.value;
+        }
+
+        static public bool operator <(Dado d1, Dado d2)
+        {
+            return !(d1 > d2);
+        }
+
+        static public bool operator ==(Dado d1, Dado d2)
+        {
+            return d1.value == d2.value;
+        }
+
+        static public bool operator !=(Dado d1, Dado d2)
+        {
+            return !(d1 == d2);
+        }
+
+    }
+}
